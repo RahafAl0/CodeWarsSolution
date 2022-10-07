8f125ac2462ea05c77498b7feac1af1ec621548f
RahafAl0 <ra.al9760@gmail.com>
2022-10-07 21:16:27 +0300

    add new solution

---
+++ 7kyu.js
@@ -0,0 +1,90 @@
+// Your task is to get Zodiac Sign using input day and month.
+const getZodiacSign = (day, month) => {
+    // good luck
+     if(month == 1){
+          if(day < 20){
+              return 'Capricorn'
+          }else{
+              return 'Aquarius'
+          }
+      }
+      if(month == 2){
+          if(day < 19){
+              return 'Aquarius'
+          }else{
+              return 'Pisces'
+          }
+      }
+      if(month == 3){
+          if(day < 21){
+              return 'Pisces'
+          }else {
+              return 'Aries'
+          }
+      }
+      if(month == 4){
+          if(day < 20){
+              return 'Aries'
+          }else {
+              return 'Taurus'
+          }
+      }
+      if(month == 5){
+          if(day < 21){
+              return 'Taurus'
+          }else {
+              return 'Gemini'
+          }
+      }
+      if(month == 6){
+          if(day < 22){
+              return 'Gemini'
+          }else{
+              return 'Cancer'
+          }
+      }
+      if(month == 7){
+          if(day < 23){
+              return 'Cancer'
+          }else{
+              return 'Leo'
+          }
+      }
+      if(month == 8){
+          if(day < 23){
+              return 'Leo'
+          }else{
+              return 'Virgo'
+          }
+      }
+      if(month == 9){
+          if(day < 23){
+              return 'Virgo'
+          }else{
+              return 'Libra'
+          }
+      }
+      if(month == 10){
+          if(day < 23){
+              return 'Libra'
+          }else{
+              return 'Scorpio'
+          }
+      }
+      if(month == 11){
+          if(day < 22){
+              return 'Scorpio'
+          }else{
+              return 'Sagittarius'
+          }
+      }
+      if(month == 12){
+          if(day < 22){
+              return 'Sagittarius'
+          }else{
+              return 'Capricorn' 
+          }
+      }
+  }
+
+  
